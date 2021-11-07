592421c63c409d07f9456f74eee03cd8e259363d
CandyDeLight <78578943+CandyDeLight@users.noreply.github.com>
2021-11-07 15:10:15 -0500

    What's the threshold?
    I'm not sure how to find the threshold.
    
    I know that if ECCE > threshold... match node V & node U.

--- privacy_project_part1.py
+++ privacy_project_part1.py
@@ -5,46 +5,92 @@
 
 
 import networkx as nx
-import matplotlib.pyplot as plt
-#you can leave these alone
+import math
+import statistics
 
 
 # In[2]:
 
 
-data_dir1 = r'C:\Users\Dan\(0) privacy\part1\\' #you might need to change to your directory
-file1 = 'seed_G1.edgelist' #you don't need to change this
+data_dir1 = r'C:\Users\Dan\(2) privacy\part1\\'
+file1 = 'seed_G1.edgelist'
+
+data_dir2 = r'C:\Users\Dan\(2) privacy\part1\\'
+file2 = 'seed_G2.edgelist'
+
+data_dir3 = r'C:\Users\Dan\(2) privacy\part1\\'
+file3 = 'seed_node_pairs.txt'
 
 
 # In[3]:
 
 
-a = nx.read_edgelist(data_dir1+file1, create_using=nx.Graph(), nodetype=int)
+G1 = nx.read_edgelist(data_dir1+file1, create_using=nx.Graph(), nodetype=int)
 
-print(nx.info(a))
+G2 = nx.read_edgelist(data_dir2+file2, create_using=nx.Graph(), nodetype=int)
 
-nx.draw(a)
+print(nx.info(G1))
+print("")
+print(nx.info(G2))
 
-plt.show()
+arrayOfNodesG1 = list(nx.nodes(G1))
+
+arrayOfNodesG2 = list(nx.nodes(G2))
+
+seedPairG1 = []
+seedPairG2 = []
+
+with open(data_dir3+file3, "r") as text_file:
+    for line in text_file:
+        line = line.strip()
+        number1 , number2 = line.split(" ")
+        seedPairG1.append(int(number1))
+        seedPairG2.append(int(number2))
+        
+unMatched = [x for x in arrayOfNodesG2 if (x not in seedPairG2)]
 
 
 # In[4]:
 
 
-data_dir2 = r'C:\Users\Dan\(0) privacy\part1\\' #you might need to change to your directory
-file2 = 'seed_G2.edgelist' #you don't need to change this
+def calc(arrayG1:list, arrayG2:list, nonMatchedG2:list):
+    for x in range(len(arrayG1)):
+        scoreList = []
+        degreeOfV = len(list(nx.neighbors(G1, x)))
+        for y in nonMatchedG2:
+            count = 0
+            degreeOfU = len(list(nx.neighbors(G2, y)))
+            neighbors = list(nx.neighbors(G2, y))
+            for nodeU in neighbors:
+                for z in seedPairG2:
+                    if nodeU == z:
+                        count = count + 1
+            score = (count)/(math.sqrt(degreeOfV) * math.sqrt(degreeOfU))
+            scoreList.append(score)
+        std = statistics.pstdev(scoreList)
+        max1 = max(scoreList)
+        scoreList.remove(max1)
+        max2 = max(scoreList)
+        ecce = (max1 - max2)/(std)
+        #print("Node V =", x)
+        #print("Scores =", scoreList)
+        #print("Std =", std)
+        #print("Max1 =", max1)
+        #print("Max2 =", max2)
+        #print("ECCE = (", max1, "-", max2, ") /", std, "=", ecce)
+        print("ECCE =", ecce)
+        #We now have the ecce.
+        #But what's the threshold? If ecce > threshold match node V (y) and U (x).
+        print(" ")
+        print("-----")
+        print(" ")
+    pass
 
 
-# In[5]:
+# In[ ]:
 
 
-b = nx.read_edgelist(data_dir2+file2, create_using=nx.Graph(), nodetype=int)
-
-print(nx.info(b))
-
-nx.draw(b)
-
-plt.show()
+calc(arrayOfNodesG1, arrayOfNodesG2, unMatched)
 
 
 # In[ ]:
